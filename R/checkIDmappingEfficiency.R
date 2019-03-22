e8dac57d617802519e6f39354f77ef55ae21fa46
LucasKookUZH <lucasheinrich.kook@uzh.ch>
2019-03-22 13:55:38 +0100

    Adding simple id mapping efficiency checker

---
+++ R/checkIDmappingEfficiency.R
@@ -0,0 +1,39 @@
+#' Checks mapping efficiency for IDs on the most commonly used ID types
+#' @param IDs character vector of IDs
+#' @param keytype corresponding keytype
+#' @importFrom AnnotationDbi mapIds
+#' @importFrom S4Vectors na.omit
+#' @importFrom dplyr distinct
+#' @return Returns a data.frame containing information of mapping efficiency on GO, ENTREZ and KEGG IDs
+#' @export checkIDmappingEfficiency
+#'
+checkIDmappingEfficiency <- function(IDs, keytype) {
+  uniqueInputIDs <- unique(IDs)
+  number_unique_InputIDs <- length(uniqueInputIDs)
+
+  mapping <- function(x = org.Hs.eg.db, column) {
+    mpd <- mapIds(
+      x = x,
+      keys = uniqueInputIDs,
+      keytype = keytype,
+      column = column,
+      multiVals = "CharacterList"
+    ) %>% unlist %>%
+      data.frame(InputIDs = names(.), mappedIDs = .) %>%
+      na.omit %>% distinct(InputIDs) %>% nrow %>% return()
+  }
+
+  ENTREZmapped <- mapping(column = "ENTREZID")
+  KEGGmapped <- mapping(column = "PATH")
+  GOmapped <- mapping(column = "GO")
+
+  out <- data.frame(InputIDs = c(number_unique_InputIDs, 100),
+       ENTREZIDs = c(ENTREZmapped, ENTREZmapped/number_unique_InputIDs*100),
+       KEGGIDs = c(KEGGmapped, KEGGmapped/number_unique_InputIDs*100),
+       GOIDs = c(GOmapped, GOmapped/number_unique_InputIDs*100)
+       )
+  row.names(out) <- c("Number", "Percent Mapped")
+  return(out)
+}
+
+# checkIDmappingEfficiency(IDs = dd$UniprotID, keytype = "UNIPROT")
