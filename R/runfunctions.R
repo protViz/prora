564e3dece90c090516711e9118662b4544adeb28
LucasKookUZH <lucasheinrich.kook@uzh.ch>
2019-06-28 10:42:07 +0200

    Adding wrappers for running on wide format data

--- R/runfunctions.R
+++ R/runfunctions.R
@@ -228,3 +228,211 @@ runWebGestaltORAlong <- function(contrast,
       projectName = fpath
     )
 }
+
+#' @export runGSEA
+runGSEA <- function(data,
+                    fpath,
+                    ID_col = "UniprotID",
+                    fc_col = "estimate",
+                    organism = "hsapiens",
+                    target = "geneontology_Biological_Process",
+                    map_col = "GO",
+                    nperm = 10,
+                    outdir = "GSEA") {
+  outdir <- paste0(outdir, "_", target)
+
+  if (!dir.exists(outdir)) {
+    dir.create(outdir)
+  }
+
+  ranktable <- data %>%
+    dplyr::select(!!sym(ID_col), Score = !!sym(fc_col))
+
+  GSEA_res <-
+    WebGestaltR(
+      enrichMethod = "GSEA",
+      organism = organism,
+      enrichDatabase = target,
+      interestGene = ranktable,
+      interestGeneType = "uniprotswissprot",
+      outputDirectory = outdir,
+      isOutput = TRUE,
+      perNum = nperm,
+      projectName = fpath
+    )
+
+  f_mappingTable <- file.path(
+    outdir,
+    paste0("Project_", fpath),
+    paste0("interestingID_mappingTable_", fpath, ".txt")
+  )
+  mappingTable <-
+    read_delim(f_mappingTable,
+               delim = "\t")
+
+  mappingTable %>% mutate(entrezgene = as.character(entrezgene)) -> mappingTable
+  GSEA_res_sep <-
+    GSEA_res %>% separate_rows(leadingEdgeId, sep = ";")
+  merged_data <- inner_join(mappingTable,
+                            GSEA_res_sep,
+                            by = c("entrezgene" = "leadingEdgeId"))
+
+  readr::write_delim(
+    merged_data,
+    path = file.path(outdir, paste0("Project_", fpath), "merged_data.tsv"),
+    delim = "\t"
+  )
+
+  GSEA <- list(
+    organism = organism,
+    target = target,
+    input_data = ranktable,
+    output_dir = fpath,
+    merged_data = merged_data,
+    nperm = nperm
+  )
+
+  rmarkdownPath <-
+    file.path(outdir, paste0("Project_", fpath), "GSEA.Rmd")
+  bibpath <-
+    file.path(outdir, paste0("Project_", fpath), "bibliography.bib")
+
+
+  file.copy(
+    file.path(find.package("fgczgseaora"), "rmarkdown_reports/GSEA.Rmd"),
+    rmarkdownPath,
+    overwrite = TRUE
+  )
+
+  file.copy(
+    file.path(
+      find.package("fgczgseaora"),
+      "rmarkdown_reports/bibliography.bib"
+    ),
+    bibpath,
+    overwrite = TRUE
+  )
+
+  rmarkdown::render(
+    rmarkdownPath,
+    bookdown::html_document2(number_sections = FALSE),
+    params = list(GSEA = GSEA),
+    clean = TRUE
+  )
+}
+
+#' @export runSIGORA
+runSIGORA <-
+  function(data,
+           fpath,
+           target = "GO",
+           fc_col = "estimate",
+           ID_col = "UniprotID",
+           fc_threshold = 0.5,
+           outdir = "sigORA",
+           greater = TRUE) {
+    outdir <- paste0(outdir, "_", target)
+
+    if (!dir.exists(outdir)) {
+      dir.create(outdir)
+    }
+
+    if (!dir.exists(file.path(outdir, fpath))) {
+      dir.create(file.path(outdir, fpath))
+    }
+
+    GPStab <-
+      makeGPS_wrappR(data[[ID_col]], target = target, dev = TRUE)
+
+    myGPSrepo <-
+      makeGPS_wrappR(data[[ID_col]], target = target)
+
+    sigora_res <-
+      sigoraWrappR(
+        fc_threshold = fc_threshold,
+        fc_col = fc_col,
+        df = data,
+        GPSrepos = myGPSrepo,
+        db = target,
+        greater_than = greater
+      )
+
+    p1 <- try(sigora_heatmap(sigora_res, GPStab))
+
+    rmarkdownPath <- file.path(outdir, fpath, "sigora.Rmd")
+
+    bibpath <- file.path(outdir, fpath, "bibliography.bib")
+
+    file.copy(
+      file.path(
+        find.package("fgczgseaora"),
+        "rmarkdown_reports/sigora.Rmd"
+      ),
+      rmarkdownPath,
+      overwrite = TRUE
+    )
+
+    file.copy(
+      file.path(
+        find.package("fgczgseaora"),
+        "rmarkdown_reports/bibliography.bib"
+      ),
+      bibpath,
+      overwrite = TRUE
+    )
+
+    rmarkdown::render(
+      rmarkdownPath,
+      bookdown::html_document2(number_sections = FALSE),
+      params = list(
+        results = sigora_res,
+        plot1 = p1,
+        GPStable = GPStab,
+        direction_greater = greater
+      ),
+      clean = TRUE
+    )
+  }
+
+
+#' @export runWebGestaltORA
+runWebGestaltORA <- function(data,
+                             fpath,
+                             organism = "hsapiens",
+                             ID_col = "UniprotID",
+                             target = "geneontology_Biological_Process",
+                             map_col = "GO",
+                             threshold = 0.5,
+                             greater = TRUE,
+                             nperm = 10,
+                             fc_col = "estimate",
+                             outdir = "WebGestalt_ORA") {
+  outdir <- paste0(outdir, "_", target)
+
+  if (!dir.exists(outdir)) {
+    dir.create(outdir)
+  }
+
+  dat <- data %>%
+    dplyr::select(!!sym(ID_col), Score = !!sym(fc_col))
+
+  ranktable <-
+    apply_threshold(df = dat,
+                    th = threshold,
+                    greater = greater)
+
+  ORA_res <-
+    WebGestaltR(
+      enrichMethod = "ORA",
+      organism = organism,
+      enrichDatabase = target,
+      interestGene = ranktable[[ID_col]],
+      referenceGene = data[[ID_col]],
+      interestGeneType = "uniprotswissprot",
+      referenceGeneType = "uniprotswissprot",
+      outputDirectory = outdir,
+      isOutput = TRUE,
+      perNum = nperm,
+      projectName = fpath
+    )
+}
