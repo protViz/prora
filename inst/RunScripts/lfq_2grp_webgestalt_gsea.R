8a41d26de4d5673b069fd35e61e4fabb75892b6d
Witold Wolski <wewolski@gmail.com>
2019-07-16 17:23:06 +0200

    webgestalt gsea for 2grp report

---
+++ inst/RunScripts/lfq_2grp_webgestalt_gsea.R
@@ -0,0 +1,71 @@
+library(WebGestaltR)
+library(tidyverse)
+library(org.Hs.eg.db)
+library(sigora)
+library(GO.db)
+library(slam)
+library(fgczgseaora)
+library(readr)
+
+# Files -------------------------------------------------------------------
+
+grp2report <- "data/2Grp_CF_a_vs_CF_b.txt"
+result_dir <- "gsea_ora_results"
+
+
+target_GSEA <- c(
+  "geneontology_Biological_Process",
+  "geneontology_Cellular_Component",
+  "geneontology_Molecular_Function"
+)
+
+
+#target_SIGORA <- target_SIGORA[1]
+# Parameters --------------------------------------------------------------
+
+organism <- "hsapiens"
+ID_col <- "UniprotID"
+fc_col <- "estimate"
+
+grp2report <- "data/2Grp_CF_a_vs_CF_b.txt"
+result_dir <- "gsea_ora_results"
+
+
+organism <- "hsapiens"
+ID_col <- "TopProteinName"
+fc_col <- "log2FC"
+nperm <- 10
+
+#
+fpath_se <- tools::file_path_sans_ext(basename(grp2report))
+odir <- file.path(result_dir , make.names(fpath_se))
+if (!dir.exists(odir)) {
+  dir.create(odir)
+}
+
+
+dd <- read_tsv(grp2report)
+dd <- dd %>% select_at(c(ID_col, fc_col))
+
+
+filtered_dd <- getUniprotFromFastaHeader(dd,idcolumn = ID_col) %>%
+  filter(!is.na(UniprotID))
+
+filtered_dd <- na.omit(filtered_dd)
+
+
+res <- lapply(target_GSEA, function(x) {
+  message(x)
+  fgczgseaora:::.runGSEA(
+    data = filtered_dd,
+    fpath = "",
+    ID_col = "UniprotID",
+    fc_col = fc_col,
+    organism = organism,
+    target = x,
+    nperm = nperm,
+    outdir = file.path(odir, "GSEA")
+  )
+})
+
+
