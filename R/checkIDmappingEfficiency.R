6e260e86f29b721049aac43a73b39490de6eeef1
LucasKookUZH <lucasheinrich.kook@uzh.ch>
2019-08-12 14:20:50 +0200

    .data throws error in checkIDmappingEfficiency()

--- R/checkIDmappingEfficiency.R
+++ R/checkIDmappingEfficiency.R
@@ -4,7 +4,6 @@
 #' @importFrom AnnotationDbi mapIds
 #' @importFrom S4Vectors na.omit
 #' @importFrom dplyr distinct
-#' @importFrom rlang .data
 #' @return Returns a data.frame containing information of mapping efficiency on GO, ENTREZ and KEGG IDs
 #'
 #' @examples
@@ -26,7 +25,7 @@ checkIDmappingEfficiency <- function(IDs, keytype) {
       column = column,
       multiVals = "CharacterList"
     ) %>% unlist %>%
-      data.frame(InputIDs = names(.data), mappedIDs = .data) %>%
+      data.frame(InputIDs = names(.), mappedIDs = .) %>%
       na.omit %>% distinct(InputIDs) %>% nrow %>% return()
   }
 
