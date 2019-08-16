ec892b281b2a3ada1fb1a780a3bb10eb5e1aa2d7
LucasKookUZH <lucasheinrich.kook@uzh.ch>
2019-08-16 11:15:35 +0200

    Fix .data issue with tibble::enframe()

--- R/checkIDmappingEfficiency.R
+++ R/checkIDmappingEfficiency.R
@@ -4,6 +4,7 @@
 #' @importFrom AnnotationDbi mapIds
 #' @importFrom S4Vectors na.omit
 #' @importFrom dplyr distinct
+#' @importFrom tibble enframe
 #' @return Returns a data.frame containing information of mapping efficiency on GO, ENTREZ and KEGG IDs
 #'
 #' @examples
@@ -25,7 +26,7 @@ checkIDmappingEfficiency <- function(IDs, keytype) {
       column = column,
       multiVals = "CharacterList"
     ) %>% unlist %>%
-      data.frame(InputIDs = names(.), mappedIDs = .) %>%
+      enframe(name = "InputIDs", value = "mappedIDs") %>%
       na.omit %>% distinct(InputIDs) %>% nrow %>% return()
   }
 
