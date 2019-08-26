0c499efbbca15001b0b601c3111e898d130c7698
Witold Wolski <wewolski@gmail.com>
2019-08-26 12:14:03 +0200

    moved to docopt

--- inst/run_scripts/lfq_2grp_webgestalt_ora.R
+++ inst/run_scripts/lfq_2grp_webgestalt_ora.R
@@ -1,40 +1,47 @@
-#!/usr/bin/env Rscript
+#!/usr/bin/Rscript
+"WebGestaltR ORA
 
-#lfq_2grp_webgestalt_ORA.R
-library(WebGestaltR)
-library(tidyverse)
-library(org.Hs.eg.db)
-library(sigora)
-library(GO.db)
-library(slam)
-library(fgczgseaora)
-library(readr)
+Usage:
+  test.R <grp2file> [--organism=<organism>] [--outdir=<outdir>]
+        [--nperm=<nperm>] [--ID_col=<ID_col>] [--fc_col=<fc_col>]
 
-# Files -------------------------------------------------------------------
-args = commandArgs(trailingOnly=TRUE)
-if (length(args) < 2) {
-  stop("At least two argument must be supplied: grp2file and output_directory", call.=FALSE)
-}
+Options:
+  -o --organism=<organism> organism [default: hsapiens]
+  -r --outdir=<outdir> output directory [default: results_ora]
+  -t --log2fc=<log2fc> fc threshold [default: 1]
+  -g --is_greater=<is_greater> is greater than log2fc [default: TRUE]
+  -n --nperm=<nperm> number of permutations to calculate enrichment scores [default: 50]
+  -i --ID_col=<ID_col> Column containing the UniprotIDs [default: TopProteinName]
+  -f --fc_col=<fc_col> Column containing the estimates [default: log2FC]
+
+Arguments:
+  grp2file  input file
+" -> doc
+
+library(docopt)
+opt <- docopt(doc)
+
+options(warn = -1)
+suppressMessages(library(WebGestaltR))
+suppressMessages(library(tidyverse))
+suppressMessages(library(org.Hs.eg.db))
+suppressMessages(library(sigora))
+suppressMessages(library(GO.db))
+suppressMessages(library(slam))
+suppressMessages(library(fgczgseaora))
+suppressMessages(library(readr))
 
 
-grp2report <- args[1]
-result_dir <- args[2]
+# Check command args ------------------------------------------------------
 
-nperm <- 10
-
-if (length(args)==3) {
-  # default output file
-  nperm <- args[3]
-}
-
-ID_col <- "TopProteinName"
-fc_col <- "log2FC"
-if (length(args) == 5) {
-  # default output file
-  ID_col <- args[4]
-  fc_col <- args[5]
-}
-
+cat("\nParameters used:\n\t grp2report:", grp2report <- opt$grp2file, "\n\t",
+    "result_dir:", result_dir <- opt[["--outdir"]], "\n\t",
+    "  organism:", organism <- opt[["--organism"]], "\n\t",
+    "    log2fc:", log2fc <- opt[["--log2fc"]], "\n\t",
+    "is_greater:", is_greater <- opt[["--is_greater"]], "\n\t",
+    "     nperm:", nperm <- as.numeric(opt[["--nperm"]]), "\n\t",
+    "    ID_col:", ID_col <- opt[["--ID_col"]],"\n\t",
+    "    fc_col:", fc_col <- opt[["--fc_col"]] , "\n\n\n")
 
 
 target_GSEA <- c(
@@ -44,38 +51,50 @@ target_GSEA <- c(
 )
 
 
+organisms <- listOrganism(hostName = "http://www.webgestalt.org/", cache = NULL)
 
-#
+if(! organism %in% organisms){
+  stop("organism : ",organism, "is not in the list of available organisms", paste(organisms, collapse=" ") )
+}
+
+# Parameters --------------------------------------------------------------
+
 fpath_se <- tools::file_path_sans_ext(basename(grp2report))
-
 odir <- file.path(result_dir , make.names(fpath_se))
+
+if(!dir.exists(result_dir)){
+  dir.create(result_dir)
+}
 
 if (!dir.exists(odir)) {
   dir.create(odir)
 }
 
+fc_estimates <- read_tsv(grp2report)
+fc_estimates <- fc_estimates %>% select_at(c(ID_col, fc_col))
 
-dd <- read_tsv(grp2report)
-dd <- dd %>% select_at(c(ID_col, fc_col))
+print("Selected columns: ")
+print(sample_n(fc_estimates, 10))
 
-
-filtered_dd <- getUniprotFromFastaHeader(dd,idcolumn = ID_col) %>%
+filtered_dd <- getUniprotFromFastaHeader(fc_estimates, idcolumn = ID_col) %>%
   filter(!is.na(UniprotID))
-
 filtered_dd <- na.omit(filtered_dd)
+print("After ID filtering columns: ")
+print(sample_n(filtered_dd, 10))
+
+
 
 
 res <- lapply(target_GSEA, function(x) {
   message("\n",x,"\n")
   fgczgseaora:::.runWebGestaltORA(
-
     data = filtered_dd,
     fpath = "",
     organism = organism,
     ID_col = "UniprotID",
     target = x,
-    threshold = fc_threshold,
-    greater = greater,
+    threshold = log2fc,
+    greater = is_greater,
     nperm = nperm,
     fc_col = fc_col,
     outdir = file.path(odir, "WebGestaltORA")
