f687972f2ca8434784b519be6c6d4ebce18e2207
Witold Wolski <wewolski@gmail.com>
2019-09-02 17:19:59 +0200

    Id mapping using uniprot mapping service

---
+++ R/make_ids_uniprot.R
@@ -0,0 +1,39 @@
+#' map_ids_uniprot
+#'
+#' Access to uniprot web API. For more detail see https://www.uniprot.org/help/uploadlists
+#'
+#' @param data data
+#' @param ID_col column name containing id's
+#' @param from id type
+#' @param to id type
+#' @param format in which format to return the mapping
+#' @export
+#'
+#' @examples
+#'
+#' fc_estimates <- fgczgseaora::exampleContrastData
+#'
+#' filtered_dd <- getUniprotFromFastaHeader(fc_estimates, idcolumn = "protein_Id")
+#' head(filtered_dd)
+#' #head(filtered_dd)
+#'
+#' map_ids_uniprot(filtered_dd)
+#'
+map_ids_uniprot <- function(data, ID_col = "UniprotID" , from =  "ACC+ID", to = "P_ENTREZGENEID", format = "tab"){
+  ids_to_map <- unique(na.omit(data[[ID_col]]))
+
+  url = "https://www.uniprot.org/uploadlists/"
+  params = list(
+    from = "ACC+ID",
+    to = "P_ENTREZGENEID",
+    format = "tab",
+    query = paste(ids_to_map, collapse = " ")
+  )
+
+  r <- httr::POST(url, body = params, encode = "form")
+  mapping <- readr::read_tsv(httr::content(r))
+  mapping <- mapping %>% dplyr::rename( !!ID_col := "From", !!to := "To" )
+  res <- right_join(mapping, data, by = ID_col)
+
+  return(res)
+}
