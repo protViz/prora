39933afd89ab48c1b5d484c15bb7693c7eeb047b
Witold Wolski <wewolski@gmail.com>
2019-11-21 15:39:55 +0100

    move sigora_upsetR to sigoraWrapper

--- R/sigoraWrappR.R
+++ R/sigoraWrappR.R
@@ -4,14 +4,14 @@
 #' Provides a wrapper for \code{sigora::sigora()} combined with \code{sigora::ora()} and
 #' returns a \code{list} used for the generation of an \code{.Rmd} report.
 #'
+#' @param data input data.frame (at least two columns, first column containing IDs,
+#' other columns numerical ranks, i.e. fold changes)
 #' @param fc_threshold fold change threshold above which (in absolute terms) a protein
 #' is considered differentially regulated
 #' @param fc_col Name of the fold change column, in case the input file contains
 #' multiple contrasts
 #' @param GPSrepos GPS repository used as background, can be
 #' generated via \code{\link{sigoraWrappR}}
-#' @param df input data.frame (at least two columns, first column containing IDs,
-#' other columns numerical ranks, i.e. fold changes)
 #' @param db database used for the generation of the GPS repository
 #' @param greater_than Logical. Whether fc_threshold should be applied as
 #' greater_than (default is \code{TRUE}) or strictly less than (\code{FALSE})
@@ -28,29 +28,30 @@
 #'    \item \code{proteinsAfterFiltering}: numeric, number of proteins after fc filtering
 #' }
 #'
+#' @importFrom sigora sigora ora
+#' @export sigoraWrappR sigoraWrappR
+#'
 #' @examples
 #' data("exampleContrastData", package = "fgczgseaora")
 #' data("idmap", package = "sigora")
 #' df <- get_UniprotID_from_fasta_header(exampleContrastData)
 #' myGPSrepo <- makeGPS_wrappR(ids = df$UniprotID)
-#' res <- sigoraWrappR(fc_col = "estimate", GPSrepos = myGPSrepo,
-#'                     df = df, fc_threshold = 0.5)
+#' res <- sigoraWrappR(df,fc_col = "estimate", GPSrepos = myGPSrepo,
+#'                      fc_threshold = 0.5)
 #'
-#' @importFrom sigora sigora ora
-#' @export sigoraWrappR sigoraWrappR
-
 sigoraWrappR <-
-  function(fc_threshold = 0.5,
-           fc_col = "",
+  function(data,
+           threshold = 0.5,
+           score_col = "",
            GPSrepos = sigora::kegH,
-           df,
            db = "",
            greater_than = TRUE,
            idmap = sigora::idmap) {
+    enriched <- NULL
     if(greater_than) {
-      enriched <- df[df[, fc_col] >= fc_threshold,]
+      enriched <- data[data[, score_col] >= threshold,]
     } else {
-      enriched <- df[df[, fc_col] < fc_threshold,]
+      enriched <- data[data[, score_col] < threshold,]
     }
     sigora_res <-
       sigora::sigora(GPSrepo = GPSrepos,
@@ -60,15 +61,60 @@ sigoraWrappR <-
     ora_res <-
       sigora::ora(geneList = enriched$UniprotID,
           GPSrepo = GPSrepos)
+
     output <- list(
       sigora = sigora_res,
       ora = ora_res,
-      fc_threshold = fc_threshold,
+      threshold = threshold,
       GPSrepository = GPSrepos,
       database = db,
-      data = df[, c("UniprotID", fc_col)],
-      proteinsAfterFiltering = nrow(df[df[, fc_col] >= fc_threshold, ]),
+      data = df[, c("UniprotID", score_col)],
+      proteinsAfterFiltering = nrow(enriched),
       direction = ifelse(greater_than, yes = "greater than", no = "less than")
     )
     return(output)
   }
+
+
+#' UpSetR wrapper for sigora results
+#'
+#' @param sigora_res Object returned by the \code{\link{sigoraWrappR}} function
+#' @param GPStable Object returned by \code{\link{makeGPS_wrappR}} function, setting \code{dev=TRUE}
+#' @param ... other parameters to \code{upset}
+#'
+#' @importFrom UpSetR fromList upset
+#' @importFrom dplyr inner_join filter select mutate
+#' @importFrom tidyr spread
+#' @importFrom S4Vectors na.omit
+#' @importFrom magrittr %>%
+#' @importFrom rlang .data
+#' @export
+sigora_upsetR <- function(sigora_res, GPStable, ...) {
+  mergeR <- function(sigora_res, GPStable) {
+    tab1 <- sigora_res$sigora$summary_results
+    tab2 <- sigora_res$data
+    colnames(tab1)[1] <- "pathwayId"
+    colnames(tab2) <- c("gene", "fc")
+    tab3 <- dplyr::inner_join(GPStable, tab1)
+    tab4 <- dplyr::inner_join(tab3, tab2) %>% filter(!!sym("Bonferroni") < 0.05)
+    return(tab4)
+  }
+
+  df <- mergeR(sigora_res, GPStable)
+
+  if(any(dim(df)==0)) return(NULL)
+
+  toplot <- df %>%
+    dplyr::select(!!sym("pathwayId"), !!sym("gene")) %>%
+    dplyr::mutate(ID = seq_len(nrow(.data))) %>%
+    tidyr::spread(!!sym("pathwayId"), !!sym("gene")) %>%
+    dplyr::select(-!!sym("ID")) %>%
+    as.list() %>%
+    lapply(na.omit)
+
+  if(length(toplot)==1) stop("UpSetR plot cannot be displayed. Only one pathway enriched.")
+
+  UpSetR::fromList(toplot) %>%
+    UpSetR::upset(mb.ratio = c(0.7, 0.3), ...=...)
+}
+
