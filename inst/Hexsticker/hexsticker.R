8984d4b0d74b730c2f1811e349bad6666aab1c27
Witold Wolski <wewolski@gmail.com>
2021-02-08 12:10:51 +0100

    added hexsticker

---
+++ inst/Hexsticker/hexsticker.R
@@ -0,0 +1,18 @@
+library(hexSticker)
+imgurl = "inst/Hexsticker/cutProra.png"
+sticker(imgurl,
+        package = "prora",
+        p_color = "red",
+        p_size = 45,
+        p_y = 1,
+        s_x = 1,
+        s_y = 1,
+        s_width = 1.1,
+        h_fill = "green", h_color = "darkgreen",
+        filename = "inst/Hexsticker/proraHex.png",
+        white_around_sticker = TRUE,
+        url	= "github.com/protViz/prora",
+        u_size = 5.2,
+        u_color = "white")
+
+
