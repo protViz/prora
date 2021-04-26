344cf206f10d912013be5ef66eaa9aa0d269ec60
Witold Wolski <wewolski@gmail.com>
2021-04-26 14:28:06 +0200

    msigdb helper

---
+++ R/msigDB_helper.R
@@ -0,0 +1,19 @@
+#' getMsigdbGenesets
+#' @export
+#' @examples
+#' #hallmark$gs_subcat <- "HALLMARK"
+#' C5 <- bind_rows( msigdbr_collections() %>% filter(gs_cat == "C5") %>% filter(grepl("^GO:", gs_subcat)),
+#'                  hallmark,
+#'                  msigdbr_collections() %>% filter(gs_subcat == "CP:KEGG") )
+#' species <- "Mus musculus"
+getMsigdbGenesets <- function(msigCollection, species){
+  genesetsC5 <- vector(mode = "list", length = nrow(msigCollection) )
+  for (i in 1:nrow(msigCollection)) {
+    genesetsC5[[i]] <- msigdbr(species = species,
+                               category = msigCollection$gs_cat[i],
+                               subcategory = msigCollection$gs_subcat[i])
+  }
+  names(genesetsC5) <- make.names(msigCollection$gs_subcat)
+  fgseaGSlist <- lapply(genesetsC5 , fgsea_msigdb)
+  return(fgseaGSlist)
+}
