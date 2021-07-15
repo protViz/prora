ae236eea6167b1c95a10a054d5b28070f7c0d607
Witold Wolski <wewolski@gmail.com>
2021-07-15 14:30:31 +0200

    script update.

--- inst/clusterProfiler/analyseTSV.R
+++ inst/clusterProfiler/analyseTSV.R
@@ -1,29 +1,43 @@
+# author W Wolski
+# this script analyses all projects in file output_MaxQuant_info_WSpecies.csv
+
 library(tidyverse)
 library(readr)
+
+
+
 tmp <- readr::read_csv("output_MaxQuant_info_WSpecies.csv")
 tmp$project_ID <-  gsub("/srv/www/htdocs/(p[0-9]{1,5})/bfabric/.*","\\1",tmp$path_to_zip)
 
-human <- tmp %>% filter(species == "human" & n_inputs > 4)
-summary(human$n_inputs)
-hist(human$n_inputs)
+human <- tmp %>% filter(species == "human" & n_inputs >= 4)
+plot(table(human$n_inputs))
 
-human$windowpaths <- gsub("/srv/www/htdocs","y:",human$path_to_zip)
+if (Sys.info()["sysname"] == "Windows") {
+  human$windowpaths <- gsub("/srv/www/htdocs", "y:", human$path_to_zip)
+  RscriptExe <- "c:/Program Files/R/R-4.1.0/bin/Rscript.exe"
+  stopifnot(file.exists(RscriptExe))
+}else{
+  RscriptExe <- "/usr/bin/Rscript"
+}
 
-for (i in 47:nrow(human)) {
+
+for (i in 1:nrow(human)) {
   path <- human$windowpaths[i]
   workunitid <- human$workunitid[i]
   projectid <- human$project_ID[i]
   if (!file.exists(path)) {
     print(path)
   } else if (TRUE) {
-    arguments <- c("runscript.R", path, "human","blubA" ,"hclustdeepsplit", workunitid, projectid)
-    res <- system2("c:/Program Files/R/R-4.0.3/bin/Rscript.exe", args = arguments)
+    arguments <- c("runscript.R", path, "human", "blubA" , "hclustdeepsplit", workunitid, projectid)
+    res <- system2(RscriptExe, args = arguments)
     stopifnot(res == 0)
-    if (TRUE) {
+
+    if (FALSE) {
       commandArgs <- function(...){return(arguments[-1])}
       commandArgs()
       source("runscript.R")
     }
+
   }
 }
 
@@ -34,8 +48,8 @@ path <- "y:/p2865/bfabric/Proteomics/MaxQuant/2020/2020-01/2020-01-13/workunit_2
 path <- "y:/p2961/bfabric/Proteomics/MaxQuant/2019/2019-06/2019-06-23/workunit_200319/1292787.zip"
 arguments <- c("runscript.R", path, "human","blubB" ,"hclustdeepsplit", "p3220", "23333")
 
-commandArgs <- function(...){return(arguments[-1])}
-commandArgs()
-source("runscript.R")
+# commandArgs <- function(...){ return(arguments[-1]) }
+# commandArgs()
+# source("runscript.R")
 
 
