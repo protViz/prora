22dd12f7a3375cd8a388e5268b33fad7ca5fd5f9
mariaderrico <maria@ninthfloor.org>
2021-08-13 11:35:06 +0200

    Add parameter metric to runDPAclustering in order to accept precomputed distance as input

--- R/cphelper.R
+++ R/cphelper.R
@@ -52,15 +52,23 @@ cp_clusterHClustEuclideanDistDeepslit <- function(x,  method = "complete", JK = 
 }
 
 #' Cluster using DPA
+#'
+#' @param mdata, input data
+#' @param Z, the number of standard deviations fixing the level of
+#'            statistical confidence at which one decides to consider
+#'            a cluster meaningful. Default value is set to 1.
+#' @param metric, string, valid metrics are "precomputed", "euclidean".
+#'            If metric is "precomputed", data is assumed to be a distance matrix.
+#'            Default is "euclidean".
 #' @export
 #'
-cp_clusterDPAEuclideanDist <- function(mdata, Z = 1, JK = TRUE){
+cp_clusterDPAEuclideanDist <- function(mdata, Z = 1, metric = "euclidean", JK = TRUE){
   distJK <- if (JK) {
     prora::dist_JK( mdata )
   } else {
     dist(mdata)
   }
-  DPAresult <- DPAclustR::runDPAclustering(as.matrix(distJK), Z = Z)
+  DPAresult <- DPAclustR::runDPAclustering(as.matrix(distJK), Z = Z, metric = metric)
   k <- DPAresult$labels
   maxD <- max(DPAresult$density)
   topography <- DPAresult$topography
