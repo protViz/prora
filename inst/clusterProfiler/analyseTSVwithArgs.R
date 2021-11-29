6a81beec60f4a9eabb413793591dacfa0186e801
Witold Wolski <wewolski@gmail.com>
2021-11-29 10:45:22 +0100

    updating runscript

--- inst/clusterProfiler/analyseTSVwithArgs.R
+++ inst/clusterProfiler/analyseTSVwithArgs.R
@@ -34,7 +34,7 @@ if (length(args) == 4) {
 tmp <- readr::read_csv("output_MaxQuant_info_WSpecies.csv")
 tmp$project_ID <-  gsub("/srv/www/htdocs/(p[0-9]{1,5})/bfabric/.*","\\1",tmp$path_to_zip)
 
-human <- tmp %>% filter(species == "human" & n_inputs >= 4)
+human <- tmp %>% filter(species == "human" & n_inputs >= 8)
 
 if (Sys.info()["sysname"] == "Windows") {
   human$windowpaths <- gsub("/srv/www/htdocs", "y:", human$path_to_zip)
@@ -50,9 +50,11 @@ if (Sys.info()["sysname"] == "Windows") {
 
 for (i in 1:nrow(human)) {
   i <- 1
+
   path <- human$windowpaths[i]
   workunitid <- human$workunitid[i]
   projectid <- human$project_ID[i]
+  message(path, "\nworkunitid : ", workunitid,"\nprojectid : ", projectid)
   if (!file.exists(path)) {
     print(path)
   } else if (TRUE) {
@@ -67,6 +69,7 @@ for (i in 1:nrow(human)) {
                    jackknife = parameters$JK)
 
     res <- system2(RscriptExe, args = arguments)
+
     if (res > 0) {
       print("ERROR START running script failed")
       cat(paste("ERROR : ", arguments ,"\n"))
@@ -79,6 +82,8 @@ for (i in 1:nrow(human)) {
 #path <- "y:/p2673/bfabric/Proteomics/MaxQuant/2021/2021-01/2021-01-19/workunit_256172/1792120.zip"
 #path <-  "y:/p2954/bfabric/Proteomics/MaxQuant/2019/2019-01/2019-01-09/workunit_187912/1068470.zip"
 #path <- "y:/p2865/bfabric/Proteomics/MaxQuant/2020/2020-01/2020-01-13/workunit_234979/1462850.zip"
+
+
 if(FALSE){
   path <- "/srv/www/htdocs/p2702/bfabric/Proteomics/MaxQuant/2019/2019-03/2019-03-14/workunit_194381/1163583.zip"
   #path <- "y:/p2961/bfabric/Proteomics/MaxQuant/2019/2019-06/2019-06-23/workunit_200319/1292787.zip"
